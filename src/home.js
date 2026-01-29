423b2f3e660d744a75127a9be645d26089e34e4e
Freddy Merino <alfimerino@gmail.com>
2026-01-29 01:17:59 -0500

    Added bottom image

--- src/home.js
+++ src/home.js
@@ -13,27 +13,28 @@ export function loadHome() {
   image.src = foodImage;
   image.classList.add("hero-image");
 
-  const heroTextContainer = document.createElement('div')
-  heroTextContainer.classList.add('heroTextContainer')
+  const heroTextContainer = document.createElement("div");
+  heroTextContainer.classList.add("heroTextContainer");
 
-  const heroTextTitle = document.createElement('span')
-  heroTextTitle.textContent = "Ari's"
-  heroTextTitle.classList.add("heroTextTitle")
+  const heroTextTitle = document.createElement("span");
+  heroTextTitle.textContent = "Ari's";
+  heroTextTitle.classList.add("heroTextTitle");
 
-  const heroTextSubtitle = document.createElement('span')
-  heroTextSubtitle.textContent = "Bar & Bistro"
-  heroTextSubtitle.classList.add("heroTextSubtitle")
+  const heroTextSubtitle = document.createElement("span");
+  heroTextSubtitle.textContent = "Bar & Bistro";
+  heroTextSubtitle.classList.add("heroTextSubtitle");
 
-  const blurbContainer = document.createElement('div')
-  blurbContainer.classList.add('blurbContainer')
-  blurbContainer.innerHTML = "Founded on the belief that dining should be an immersive escape, Ari's Restaurant & Bistro blends culinary precision with a rebellious spirit. Our kitchen focuses on locally sourced ingredients transformed through fire and imagination.<br><br>From the first pour of a craft cocktail to the final bite of our signature dessert, every detail is choreographed to create a memory. Whether you're here for a quiet date under our neon glow or a spirited celebration with friends, you're home."
+  const blurbContainer = document.createElement("div");
+  blurbContainer.classList.add("blurbContainer");
+  blurbContainer.innerHTML =
+    "Founded on the belief that dining should be an immersive escape, Ari's Restaurant & Bistro blends culinary precision with a rebellious spirit. Our kitchen focuses on locally sourced ingredients transformed through fire and imagination.<br><br>From the first pour of a craft cocktail to the final bite of our signature dessert, every detail is choreographed to create a memory. Whether you're here for a quiet date under our neon glow or a spirited celebration with friends, you're home.";
 
+  const bottomImage = document.createElement("img");
+  bottomImage.src = heroImage;
+  bottomImage.classList.add("bottomImage");
 
-
-
-
-  heroTextContainer.append(heroTextTitle, heroTextSubtitle)
+  heroTextContainer.append(heroTextTitle, heroTextSubtitle);
   hero.append(image, heroTextContainer);
 
-  content.append(hero, blurbContainer);
+  content.append(hero, blurbContainer, bottomImage);
 }
