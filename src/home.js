a2044a6972f98f898d32c84ece3dd0afe1c8cfe1
Freddy Merino <alfimerino@gmail.com>
2026-01-29 01:06:30 -0500

    Added blurb section and content

--- src/home.js
+++ src/home.js
@@ -16,7 +16,6 @@ export function loadHome() {
   const heroTextContainer = document.createElement('div')
   heroTextContainer.classList.add('heroTextContainer')
 
-
   const heroTextTitle = document.createElement('span')
   heroTextTitle.textContent = "Ari's"
   heroTextTitle.classList.add("heroTextTitle")
@@ -25,8 +24,16 @@ export function loadHome() {
   heroTextSubtitle.textContent = "Bar & Bistro"
   heroTextSubtitle.classList.add("heroTextSubtitle")
 
+  const blurbContainer = document.createElement('div')
+  blurbContainer.classList.add('blurbContainer')
+  blurbContainer.innerHTML = "Founded on the belief that dining should be an immersive escape, Ari's Restaurant & Bistro blends culinary precision with a rebellious spirit. Our kitchen focuses on locally sourced ingredients transformed through fire and imagination.<br><br>From the first pour of a craft cocktail to the final bite of our signature dessert, every detail is choreographed to create a memory. Whether you're here for a quiet date under our neon glow or a spirited celebration with friends, you're home."
+
+
+
+
+
   heroTextContainer.append(heroTextTitle, heroTextSubtitle)
   hero.append(image, heroTextContainer);
 
-  content.appendChild(hero);
+  content.append(hero, blurbContainer);
 }
