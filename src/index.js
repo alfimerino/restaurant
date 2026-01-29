f1bf1799616b6251bc02a80258b3ba696645ee20
Freddy Merino <alfimerino@gmail.com>
2026-01-28 23:50:34 -0500

    Added more nav links and res button

--- src/index.js
+++ src/index.js
@@ -3,18 +3,31 @@
 import "./styles.css";
 import { loadHome } from "./home.js";
 const navigation = document.querySelector(".navigation");
+navigation.classList.add("navigation");
 
 const headLine = document.createElement("h1");
 headLine.textContent = "Ari's";
 
 const navLinksContainer = document.createElement("div");
-navLinksContainer.classList.add("navigation");
+navLinksContainer.classList.add("navLinksContainer");
 
 const homeLink = document.createElement("span");
 homeLink.textContent = "Home";
-homeLink.classList.add("homeLink");
+homeLink.classList.add("navLinks");
 
-navLinksContainer.appendChild(homeLink);
+const menuLink = document.createElement("span");
+menuLink.textContent = "Menu";
+menuLink.classList.add("navLinks");
+
+const contactLink = document.createElement("span");
+contactLink.textContent = "Contact";
+contactLink.classList.add("navLinks");
+
+const resButton = document.createElement("button");
+resButton.textContent = "Reservations"
+resButton.classList.add('resButton')
+
+navLinksContainer.append(homeLink, menuLink, contactLink, resButton);
 navigation.append(headLine, navLinksContainer);
 loadHome();
 console.log("Test ok!");
